8102c2a202f27169bff8fd7141d6e3fd6cb4f743
Huong <huong.pham@rockship.co>
2018-10-15 10:04:04 +0700

    init project

---
+++ app/src/main/java/com/huong/appcinema/models/Cinema.kt
@@ -0,0 +1,8 @@
+package com.huong.appcinema.models
+
+/**
+ * Created by HuongPN on 10/15/2018.
+ */
+data class Cinema(val name: String,
+                          val ver: String,
+                          val api: String)
